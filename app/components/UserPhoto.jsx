ce0260d46c92d91026da0516e8e376d0629bd476
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-09 23:52:42 +0200

    change UserPhoto page dimension

--- app/components/UserPhoto.jsx
+++ app/components/UserPhoto.jsx
@@ -6,7 +6,7 @@ import PhotoEachEle from './PhotoEachEle';
 
 function UserPhoto() {
     return (
-        <Grid container bgcolor={green['400']} width={{ xs: '95%', md: '70%' }} height={{ xs: '70dvh', md: '80dvh' }} overflow={'auto'} margin={'50px auto'} justifyContent={'start'} alignItems={'start'} rowSpacing={'20px'} columnSpacing={'10px'} padding={1} borderRadius={'5px'}>
+        <Grid container bgcolor={green['400']} width={{ xs: '95%', md: '70%' }} height={{ xs: '50dvh', md: '60dvh' }} overflow={'auto'} margin={'10px auto'} justifyContent={'start'} alignItems={'start'} rowSpacing={'20px'} columnSpacing={'10px'} padding={1} borderRadius={'5px'}>
             <Grid item xs={12}>
                 <Typography variant='h6' component={'h2'} margin={'auto'} textAlign={'center'} border={'1px solid white'} color={'white'} borderRadius={'5px'} width={{ md: '30%' }} padding={1}>Edit Photos</Typography>
             </Grid>
