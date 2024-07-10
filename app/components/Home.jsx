768e79dcf887a09c2536b2d0a59ff77b9c2bfbee
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-10 10:56:05 +0200

    done with section that allow users or guests to see last photos

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,11 +1,19 @@
-import { Typography } from '@mui/material'
+import { Grid } from '@mui/material'
+import { green } from '@mui/material/colors'
 import React from 'react'
+import HomeLastPhotos from './HomeLastPhotos'
 
 function Home() {
     return (
-        <Typography sx={{ minHeight: '70dvh' }}>
-            ds;lkf
-        </Typography>
+        <Grid container overflow={'auto'} bgcolor={green['400']} width={{ xs: '95%', md: '75%' }} maxHeight={{ xs: '60dvh', md: '70dvh' }} margin={'auto'} spacing={2} padding={1} borderRadius={'5px'} >
+            <HomeLastPhotos />
+            <HomeLastPhotos />
+            <HomeLastPhotos />
+            <HomeLastPhotos />
+            <HomeLastPhotos />
+            <HomeLastPhotos />
+            <HomeLastPhotos />
+        </Grid>
     )
 }
 
---
+++ app/components/HomeLastPhotos.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Grid } from '@mui/material';
+
+
+export default function HomeLastPhotos() {
+    return (
+        <Grid item xs={12} sm={6} md={4} xl={3}>
+            <Card sx={{ maxWidth: 345, height: '450px' }}>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                            R
+                        </Avatar>
+                    }
+                    title="Shrimp and Chorizo Paella"
+                    subheader="September 14, 2016"
+                />
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image="/static/images/cards/paella.jpg"
+                    alt="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        This impressive paella is a perfect party dish and a fun meal to cook
+                        together with your guests. Add 1 cup of frozen peas along with the mussels,
+                        if you like.
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <FavoriteIcon color='disabled' />
+                    <Typography component={'h3'}>55</Typography>
+                </CardActions>
+            </Card>
+        </Grid>
+    );
+}
