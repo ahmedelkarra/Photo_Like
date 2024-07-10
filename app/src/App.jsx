8c21f07bd542d97948870755c85b6f264f0fa5a5
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-10 21:11:30 +0200

    done with create context for isChange userInfo isUser state

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -8,22 +8,35 @@ import Home from '../components/Home';
 import UserEdit from '../components/UserEdit';
 import UserPhoto from '../components/UserPhoto';
 import UserMe from '../components/UserMe';
+import { IsUser } from '../context/IsUser';
+import { IsChange } from '../context/IsChange';
+import { UserInfo } from '../context/UserInfo';
+import { useState } from 'react';
 
 function App() {
+  const [isUser, setIsUser] = useState(false)
+  const [isChange, setIsChange] = useState(false)
+  const [userInfo, setUserInfo] = useState({ _id: '', fName: '', lName: '', email: '' })
   return (
     <BrowserRouter>
       <Typography component={'div'} sx={{ position: 'relative', display: 'flex', flexDirection: 'column', minHeight: '100dvh', justifyContent: 'space-between' }}>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/me' element={<UserMe />} >
-            <Route path='edit' element={<UserEdit />} />
-            <Route path='photo' element={<UserPhoto />} />
-          </Route>
-        </Routes>
-        <Footer />
+        <IsUser.Provider value={{ isUser, setIsUser }}>
+          <IsChange.Provider value={{ isChange, setIsChange }}>
+            <UserInfo.Provider value={{ userInfo, setUserInfo }}>
+              <Header />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/me' element={<UserMe />} >
+                  <Route path='edit' element={<UserEdit />} />
+                  <Route path='photo' element={<UserPhoto />} />
+                </Route>
+              </Routes>
+              <Footer />
+            </UserInfo.Provider>
+          </IsChange.Provider>
+        </IsUser.Provider>
       </Typography>
     </BrowserRouter>
   )
