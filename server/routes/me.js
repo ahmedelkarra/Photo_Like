b451e9a3634d72af202d7eae3fb44352801eae4d
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-11 17:38:12 +0200

    fix me route found duplicate logic

--- server/routes/me.js
+++ server/routes/me.js
@@ -8,22 +8,12 @@ router.get('/me', async (req, res) => {
     const data = tokenVerify(token)
     if (data) {
         try {
-            if (data) {
-                try {
-                    const user = await UserSchema.findOne(data.user).select(['_id', 'fName', 'lName', 'email', 'pass'])
-                    user.pass = undefined
-                    if (user) {
-                        res.status(200).json({ message: user })
-                    } else {
-                        res.status(403).json({ message: 'Invalid token' })
-                    }
-                } catch (error) {
-                    res.status(400).json({ message: 'Something went wrong' })
-                }
-            } else if (data == false) {
-                res.status(403).json({ message: 'Invalid token' })
+            const user = await UserSchema.findOne(data.user).select(['_id', 'fName', 'lName', 'email', 'pass'])
+            user.pass = undefined
+            if (user) {
+                res.status(200).json({ message: user })
             } else {
-                res.status(400).json({ message: 'Please check your inputs' })
+                res.status(403).json({ message: 'Invalid token' })
             }
         } catch (error) {
             res.status(400).json({ message: 'Something went wrong' })
