545e723abc8f776d1bc1f9f778e1e727b35b25c5
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-12 17:30:16 +0200

    done with control route that can update photo information

--- server/routes/control.js
+++ server/routes/control.js
@@ -1,12 +1,25 @@
 const router = require('express').Router()
+const isUser = require('../middleware/isUser')
+const PhotoSchema = require('../models/photoSchema')
 
 
 
-router.put('/control', function (req, res) {
-    res.send('Control put Route');
+
+router.put('/control', isUser, async function (req, res) {
+    const { _id, title, body } = req.body
+    try {
+        const userPhotos = await PhotoSchema.findByIdAndUpdate(_id, { title, body })
+        res.status(200).json({ message: 'Photo has been updated' })
+    } catch (error) {
+        res.status(404).json({ message: 'Photo not found' })
+    }
 });
 
-router.delete('/control', function (req, res) {
+
+
+
+
+router.delete('/control', isUser, async function (req, res) {
     res.send('Control delete Route');
 });
 
