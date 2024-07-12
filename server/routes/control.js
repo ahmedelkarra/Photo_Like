096ebe662fca0d1c86470198a0195994030aa586
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-12 17:35:13 +0200

    update control route to get id by param

--- server/routes/control.js
+++ server/routes/control.js
@@ -5,10 +5,11 @@ const PhotoSchema = require('../models/photoSchema')
 
 
 
-router.put('/control', isUser, async function (req, res) {
-    const { _id, title, body } = req.body
+router.put('/control/:id', isUser, async function (req, res) {
+    const { title, body } = req.body
+    const id = req.params.id
     try {
-        const userPhotos = await PhotoSchema.findByIdAndUpdate(_id, { title, body })
+        await PhotoSchema.findByIdAndUpdate(id, { title, body })
         res.status(200).json({ message: 'Photo has been updated' })
     } catch (error) {
         res.status(404).json({ message: 'Photo not found' })
@@ -19,8 +20,14 @@ router.put('/control', isUser, async function (req, res) {
 
 
 
-router.delete('/control', isUser, async function (req, res) {
-    res.send('Control delete Route');
+router.delete('/control/:id', isUser, async function (req, res) {
+    const id = req.params.id
+    try {
+        await PhotoSchema.findByIdAndDelete(id)
+        res.status(200).json({ message: 'Photo has been updated' })
+    } catch (error) {
+        res.status(404).json({ message: 'Photo not found' })
+    }
 });
 
 module.exports = router;
