9f6e5a29664d30d5dfb6605ae72eccbcd14745eb
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-12 13:25:10 +0200

    done with multer uploading and validation file to accepted only png and jepg

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,9 +1,42 @@
 const router = require('express').Router()
+const multer = require('multer')
 
+const storage = multer.diskStorage(
+    {
+        filename: (req, file, cb) => {
+            const fileExt = file.originalname.split('.')
+            const ext = fileExt[fileExt.length - 1]
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+            cb(null, file.fieldname + '-' + uniqueSuffix + `.${ext}`)
+        },
+        destination: (req, file, cb) => {
+            cb(null, 'upload/')
+        }
+    }
+)
+
+const upload = multer({
+    storage: storage, fileFilter: (req, file, cb) => {
+        if (file.mimetype == 'image/png' || file.mimetype == 'image/jpeg') {
+            cb(null, true)
+        } else {
+            cb(null, false)
+            cb(new Error('Only png or jpeg are accepted for uploading'))
+        }
+    }
+}).single('image')
 
 
 router.post('/upload', function (req, res) {
-    res.send('Upload post Route');
+    upload(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            res.status(400).json({ message: err.message })
+        } else if (err) {
+            res.status(403).json({ message: err.message })
+        } else {
+            res.status(201).json({ message: 'Photo has been uploaded' })
+        }
+    })
 });
 
 module.exports = router;
