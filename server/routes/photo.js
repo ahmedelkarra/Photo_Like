481f08b306e1d0724b3c5137e8ab499bfdaedeb2
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-12 17:20:21 +0200

    done with photo route that can return all photo that client has

--- server/routes/photo.js
+++ server/routes/photo.js
@@ -1,9 +1,16 @@
 const router = require('express').Router()
+const isUser = require('../middleware/isUser')
+const PhotoSchema = require('../models/photoSchema')
 
 
-
-router.get('/', function (req, res) {
-    res.send('Photos get Route');
+router.get('/', isUser, async function (req, res) {
+    const { _id } = req.userInfo
+    try {
+        const userPhotos = await PhotoSchema.find({ author: _id })
+        res.status(200).json({ message: userPhotos })
+    } catch (error) {
+        res.status(404).json({ message: 'User not found' })
+    }
 });
 
 module.exports = router;
