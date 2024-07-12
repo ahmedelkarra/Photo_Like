ebd63be7abcef66cae87e0a89517c19ac115ed0b
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-12 14:29:56 +0200

    set static on route /image for view the image client has uploaded

--- server/app.js
+++ server/app.js
@@ -31,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/image', express.static(path.join(__dirname, 'upload')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
