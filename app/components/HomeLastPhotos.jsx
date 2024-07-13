40f96764cfa353236122260a7a94e7981a442009
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-13 18:07:55 +0200

    done handle likes for photos for clients they can add like for guests redirect to login page

--- app/components/HomeLastPhotos.jsx
+++ app/components/HomeLastPhotos.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
@@ -10,23 +9,33 @@ import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { Grid, IconButton } from '@mui/material';
 import HomeLastPhotosZoom from './HomeLastPhotosZoom';
-import { UserInfo } from '../context/UserInfo';
+import { IsUser } from '../context/IsUser';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function HomeLastPhotos({ photoInfo }) {
     const date = new Date(photoInfo?.createdAt)
+    const { isUser, setIsUser } = React.useContext(IsUser)
     const formattedDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-    const { userInfo, setUserInfo } = React.useContext(UserInfo)
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        if (isUser) {
+            console.log('like');
+        } else {
+            navigate('/login')
+        }
+    }
     return (
         <Grid item xs={12} sm={6} md={4} xl={3}>
             <Card sx={{ maxWidth: 345, height: '450px', textTransform: 'capitalize' }}>
                 <CardHeader
                     avatar={
                         <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                            {userInfo?.fName[0]}
+                            {photoInfo?.fName[0]}
                         </Avatar>
                     }
-                    title={userInfo?.fName + ' ' + userInfo?.lName}
+                    title={photoInfo?.fName + ' ' + photoInfo?.lName}
                     subheader={formattedDate}
                 />
 
@@ -41,10 +50,10 @@ export default function HomeLastPhotos({ photoInfo }) {
                     </Typography>
                 </CardContent>
                 <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites" onClick={() => console.log('LIKE')}>
+                    <IconButton aria-label="add to favorites" onClick={handleClick}>
                         <FavoriteIcon color='action' />
                     </IconButton>
-                    <Typography component={'h3'} margin={'0 5px'}>55</Typography>
+                    <Typography component={'h3'} margin={'0 1px'}>55</Typography>
                 </CardActions>
             </Card>
         </Grid>
