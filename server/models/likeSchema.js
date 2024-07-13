1d5492dd8a25bd20db97287ae37e4e8f185d0753
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-13 23:28:27 +0200

    correct likeSchema and add 2 methods in like route get for all likes and post to add like and remove like if the client has already liked

--- server/models/likeSchema.js
+++ server/models/likeSchema.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose')
 const likeSchema = new mongoose.Schema(
     {
         author: { type: String, required: true, ref: 'User' },
-        photo: { type: String, required: true, ref: 'Photo' },
+        photoId: { type: String, required: true, ref: 'Photo' },
     }, { timestamps: true }
 )
 
-const LikeSchema = mongoose.model('Photo', likeSchema)
+const LikeSchema = mongoose.model('likes', likeSchema)
 
 module.exports = LikeSchema
--- server/routes/like.js
+++ server/routes/like.js
@@ -1,17 +1,50 @@
 const router = require('express').Router()
+const isUser = require('../middleware/isUser');
+const LikeSchema = require('../models/likeSchema');
+const PhotoSchema = require('../models/photoSchema');
 
 
-
-router.get('/like', function (req, res) {
-    res.send('Likes get Route');
+router.get('/like', async function (req, res) {
+    try {
+        const likes = await LikeSchema.find({})
+        if (likes) {
+            res.status(200).json({ message: likes })
+        } else {
+            res.status(404).json({ message: 'Photo not found' })
+        }
+    } catch (error) {
+        res.status(404).json({ message: 'Photo not found' })
+    }
 });
 
-router.post('/like', function (req, res) {
-    res.send('Likes post Route');
+router.post('/like/:id', isUser, async function (req, res) {
+    const { _id } = req.userInfo
+    const photoId = req.params.id
+
+    if (_id && photoId) {
+        try {
+            const photo = await PhotoSchema.findById(photoId)
+            if (photo) {
+                const isLiked = await LikeSchema.findOne({ photoId: photoId, author: _id })
+                if (isLiked) {
+                    await LikeSchema.findOneAndDelete({ photoId: photoId, author: _id })
+                    res.status(200).json({ message: 'Like has been deleted' })
+                } else {
+                    const likes = await LikeSchema.create({ author: _id, photoId: photoId })
+                    if (likes) {
+                        res.status(201).json({ message: 'Like has been added' })
+                    } else {
+                        res.status(403).json({ message: 'You can not add like' })
+                    }
+                }
+            }
+        } catch (error) {
+            res.status(404).json({ message: 'Photo not found' })
+        }
+    } else {
+        res.status(400).json({ message: 'Something went wrong' })
+    }
 });
 
-router.delete('/like', function (req, res) {
-    res.send('Likes delete Route');
-});
 
 module.exports = router;
