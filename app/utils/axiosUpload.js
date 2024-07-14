0aac350ce31850d9e4573330a4353d056849d8e6
ahmedelkarra <ahmedelkarra@gmail.com>
2024-07-14 19:59:24 +0200

    add heroku url server

--- app/utils/axiosUpload.js
+++ app/utils/axiosUpload.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const host = 'http://localhost:5000/api'
+const host = 'https://photo-like-server-dcae478b8a90.herokuapp.com/api'
 
 export const axiosUpload = axios.create(
     {
